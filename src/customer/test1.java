a580d0b32c44c4b24b502fedf794f77fa10c692e
USER <USER@DESKTOP-U0VH7U8>
2023-09-05 12:34:57 +0530

    my first commit

---
+++ src/customer/test1.java
@@ -0,0 +1,39 @@
+package customer;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class test1 
+{
+	@BeforeMethod
+	public void login()
+	{
+		System.out.println("login to app");
+	}
+	
+	@AfterMethod
+	public void logout()
+	{
+		System.out.println("logout from app");
+	}
+	
+	@Test
+	public void createcustomer()
+	{
+		System.out.println("create customer");
+	}
+	
+	@Test
+	public void deletecustomer()
+	{
+		System.out.println("delete customer");
+	}
+	
+	@Test
+	public void modifycustomer()
+	{
+		System.out.println("modify customer");
+	}
+
+}
